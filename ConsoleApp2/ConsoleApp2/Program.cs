03810e7b016fe4b6501d9ff6717be8b070483e9d
GhostMan15 <46796299+GhostMan15@users.noreply.github.com>
2023-12-02 23:38:49 +0100

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using System.Collections;
+
+namespace Vaja5
+{
+    class Pes
+    {
+        private string ime;
+        private int teza;
+
+        public void Izpis(string i, int t)
+        {
+            this.ime = i;
+            this.teza = t;             
+        }
+
+    }
+    class Dalmatinec
+    {
+        private int stPik;
+        public void Nastavi(int stPik, int t, string i)
+        {
+            base.Nastavi(i, t);
+
+            i = new string("Roni");
+            t = new int ();
+
+            stPik = 500;
+
+        }
+    }
+    class Glavni
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+}
