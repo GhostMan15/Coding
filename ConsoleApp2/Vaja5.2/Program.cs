96e1582ce535496eae64ea41b7a7030e9df393d4
GhostMan15 <farukuzicanin1@gmail.com>
2023-12-03 12:48:36 +0100

    Vaja 5.2 narjena

--- ConsoleApp2/Vaja5.2/Program.cs
+++ ConsoleApp2/Vaja5.2/Program.cs
@@ -1,3 +1,38 @@
-﻿
+﻿using System;
+namespace ConsoleApp2
+{
+    public class Krog
+    { 
+        public double Premer { get; set; }
+        public Krog(double premer)
+        {
+            Premer = premer;
+        }
+        public double Povrsina()
+        {
+            double polmer = Premer / 2;
+            double povrsina = Math.PI * Math.Pow(polmer, 2);
+            return polmer;
+        }
 
+    }
+    class Krogla : Krog
+    {
+        public Krogla(double premer) : base(premer) 
+        {
 
+        }
+        public new double Povrsina()
+        {
+            double polmer = Premer / 2;
+            double povrsina = 4*Math.PI * Math.Pow(polmer, 2);
+            return povrsina;
+        }
+        public new double Prostornina()
+        {
+            double polmer = Premer / 2;
+            double prostornina = (4.0 / 3.0) * Math.PI * Math.Pow(polmer, 3);
+            return prostornina;
+        }
+    }
+}
