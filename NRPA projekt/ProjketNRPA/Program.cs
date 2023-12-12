0a77fa3d6b8d61cecc8193ffa682e33fe40a302e
GhostMan15 <farukuzicanin1@gmail.com>
2023-12-12 09:27:35 +0100

    Baje dokoncan?

--- NRPA projekt/ProjketNRPA/Program.cs
+++ NRPA projekt/ProjketNRPA/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Buffers;
+using Projekt;
 
 namespace Projekt
 {
@@ -9,29 +10,43 @@ namespace Projekt
         public int Starost { get; set; }
 
 
-          public  Oseba + (Oseba oseba 1, Oseba, oseba 2)
-	  {
+          public  Oseba + (Oseba oseba 1, Oseba, oseba 2) //Preoblikovanje operatorjev (Ugibam?)
+	    {
 		  return Oseba ($"Uporabik: {oseba1.Ime} in {oseba2.Ime};
-	  }
+	    }
         ~Oseba()
         {
             Console.WriteLine($"Oseba {Ime} je zbrisana");   
         }
+
+	
     }
 
 
 
     class Uporabnik : Oseba  
     {
-	    
+        public virtual void PredstaviSe()
+        {
+            Console.WriteLine($"Ime:{Ime}/n Starost:{Starost}/n Tip porabnika:Navadni uporabnik ");
+        }
+	   
         public void DodajTransakcijo(Transakcija transakcija) //Dodajanje transakcij uporabniku
         {
 
-       }
+        }
 
         public virtual void PrikaziPodatke()
         {
             
+        }
+    }
+
+    class Zaposleni : Oseba  //Ima pogled v uporabnike in njihove transakcije
+    {
+        public override void PredstaviSe()
+        {
+            Console.WriteLine($"Ime:{Ime}/n Starost:{Starost}/n Tip porabnika: Zaposleni ");
         }
     }
 
