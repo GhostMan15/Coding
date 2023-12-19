98c32913b28a89c8f6cf39762ab8507ffeeb3cce
GhostMan15 <farukuzicanin1@gmail.com>
2023-12-19 19:39:08 +0100

    Jutr nadaljujem

--- ProjketFroms/ProjketFroms/Form1.cs
+++ ProjketFroms/ProjketFroms/Form1.cs
@@ -1,51 +1,18 @@
 namespace ProjketFroms
 {
-    public partial class Form1 : Form
+    public partial class Form1 : Form 
     {
         public Form1()
         {
             InitializeComponent();
+
         }
-        class Oseba
-        {
-            string ime { get; set; }
-            int pin { get; set; }
 
-            string id { get; set; }
-
-            public Oseba(string ime, int pin, string id)
+            private void textBox1_TextChanged(object sender, EventArgs e)
             {
-                this.id = id;
-                this.ime = ime;
-                this.pin = pin;
-            } 
-        }
-
-        class Uporabnik : Oseba
-        {
-
-        }
-
-        class Zaposleni
-        {
-
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
+               
+            }
         
-            static void Main(string[] args)
-            {
-
-            List<Oseba> osebe = new List<Oseba>()
-                 {
-                   new Oseba("Duško", 1234, "SI78965DD"),
-                   
-                   
-                 };
-        }
+       
     }
 }
--- ProjketFroms/ProjketFroms/Program.cs
+++ ProjketFroms/ProjketFroms/Program.cs
@@ -1,10 +1,40 @@
 namespace ProjketFroms
 {
+
+    class Oseba
+    {
+        string ime { get; set; }
+        int pin { get; set; }
+
+        string id { get; set; }
+
+        public Oseba(string ime, int pin, string id)
+        {
+            this.id = id;
+            this.ime = ime;
+            this.pin = pin;
+        }
+    }
+
+    class Uporabnik 
+    {
+
+    }
+
+    class Zaposleni
+    {
+
+    }
+
+
+    
+
+
+  
+        
     internal static class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
+      
         [STAThread]
         static void Main()
         {
@@ -12,6 +42,14 @@ namespace ProjketFroms
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
+
+
+            List<Oseba> osebe = new List<Oseba>()
+            {
+                   new Oseba("Duško", 1234, "SI78965DD"),
+
+
+            };
         }
     }
 }
