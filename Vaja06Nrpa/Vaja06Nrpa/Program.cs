aff7f75f0ec21404c926558d15ad43a8578fdac1
GhostMan15 <farukuzicanin1@gmail.com>
2024-02-13 18:03:26 +0100

    mal vadu

---
+++ Vaja06Nrpa/Vaja06Nrpa/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Vaja06Nrpa;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+abstract class Liki
+{
+    public abstract double Ploscina(int x);
+    public abstract double Obseg(int y);
+
+
+}
+
+class  Krog : Liki
+{
+    public override double Ploscina(int x)
+    {
+        x = 22;
+        return x ;
+    }
+
+   public override double Obseg(int y)
+    {
+        return y.ToString();
+    }
+}
