a889032bfaa79569e4ea5044be61804fbd3e48e0
GhostMan15 <farukuzicanin1@gmail.com>
2024-03-10 21:14:06 +0100

    koncano

--- JS_smv/logic.js
+++ JS_smv/logic.js
@@ -1,24 +1,23 @@
 let velikost;
-let stevilke = [];
 let izzrebaneStevilke = [];
 let izbranaTabela = null;
+let interval;
 function omogociIgro() {
     velikost = Number(document.getElementById("steviloKartic").value);
     if (velikost == 0) {
         document.getElementById("novaIgra").disabled = true;
         document.getElementById("naprej").disabled = true;
-        //document.getElementById("igralnoPolje").innerHTML='';
     } else {
         document.getElementById("novaIgra").disabled = false;
         document.getElementById("naprej").disabled = false;
         racunalnikIzbere();
     }
 }
-
 function pozeniIgro() {
     let stVrstic = Number(document.getElementById("steviloKartic").value);
     let podatkiDiv = document.getElementById("podatki");
     pozeniTimer();
+    zvok('start.wav');
     podatkiDiv.innerHTML = '';
     izzrebaneStevilke = [];
     if (stVrstic === 3) {
@@ -31,7 +30,7 @@ function pozeniIgro() {
                     let st = i * 15 + j * 5 +stolp + 1;
                     if (st <= 75) {
                         tabela += '<td>' + st + '</td>';
-                        stevilke.push(st);
+
                     }
                 }
                 tabela += '</tr>';
@@ -51,7 +50,7 @@ function pozeniIgro() {
                     let st = i * 20 + j * 5 + k + 1;
                     if (st <= 80) {
                         tabela += '<td>' + st + '</td>';
-                        stevilke.push(st);
+
                     }
                 }
                 tabela += '</tr>';
@@ -71,25 +70,20 @@ function pozeniIgro() {
                     let st = i * 25 + j * 5 + k +1;
                     if (st <= 75) {
                         tabela += '<td>' + st + '</td>';
-                        stevilke.push(st);
+
                     }
                 }
                 tabela += '</tr>';
             }
-
             tabela += '</table>';
             tabela += '<br/>'
-
         }
-
-
         podatkiDiv.innerHTML += tabela;
     }
 
     racunalnikIzbere();
     racunalnikKopira();
     dodajanje();
-    zvok('audio.mp3');
 }
 
 function kopirajTabelo(tab) {
@@ -140,28 +134,6 @@ function racunalnikKopira()
     }
 }
 
-let interval;
-function pozeniTimer(){
-    interval = setInterval(function(){izpisCasa();}, 1000);
-    let klik_aktiven=1;
-}
-let klik_aktiven=0;
-function ustaviTimer(){
-    clearInterval(interval);
-    let klik_aktiven=0;
-}
-
-let cas = 0;
-function izpisCasa(){
-    cas++;
-    let sec = cas % 60;
-    let  min = (cas - sec)/60;
-    min += ':'
-    if(sec < 10)min+='0';
-    min += sec;
-    document.getElementById('casIgranja').innerHTML = min;
-}
-
 function  genereiraj() {
     let stevila = document.querySelectorAll("#podatki table td:not(.izbran)");
     if (stevila.length === 0) {
@@ -183,13 +155,13 @@ function  genereiraj() {
     document.getElementById("novoStevilo").textContent = izbranaStevilka;
     izzrebaneStevilke.push(parseInt(izbranaStevilka));
     let racunalnikCelice = document.querySelectorAll("#racunalnik table td");
-    let consecutiveCount = 0;
+    let stej = 0;
     racunalnikCelice.forEach(cell => {
         if (cell.textContent === izbranaStevilka && !cell.classList.contains("izbran")) {
-            cell.style.backgroundColor = "green"; // Change background color if number matches
+            cell.style.backgroundColor = "green"; // Spremeni barvo ozadja
             cell.classList.add("izbran");
-            consecutiveCount++;
-            if(consecutiveCount ===5)
+            stej++;
+            if(stej ===5)
             {
                let racunalnikZ=preveriPetZaporednih("racunalnik","green");
                 if(racunalnikZ)
@@ -199,7 +171,7 @@ function  genereiraj() {
             }
         }
         else {
-            consecutiveCount =0;
+            stej =0;
         }
         });
 
@@ -220,51 +192,29 @@ function oznaciCelico() {
 
 function preveriZmago() {
     let igralecZmaga = preveriPetZaporednih("igralec", "yellow");
-    let racunalnikZmaga = preveriPetZaporednih("racunalnik", "green");
     if (igralecZmaga) {
         let oznaceneCelice = document.querySelectorAll("#igralec table td.yellow");
-        let allNumbersPicked = true;
+        let izzreban = true;
         oznaceneCelice.forEach(function(cell) {
             let number = parseInt(cell.textContent);
             if (!izzrebaneStevilke.includes(number)) {
-                allNumbersPicked = false;
+               izzreban = false;
 
             }
         });
-        if (allNumbersPicked) {
+        if (izzreban) {
             zvok('bingo.wav');
             alert("Čestitke, zmagali ste!");
 
         }
-
     }
-    if (racunalnikZmaga) {
-        let oznaceneCElice = document.querySelectorAll("#racunalnik table td.green");
-        let racunalnikStevilke = [];
-
-        oznaceneCElice.forEach(function(cell) {
-            let number = parseInt(cell.textContent);
-            racunalnikStevilke.push(number);
-        });
-
-        let allNumbersDrawn = izzrebaneStevilke.every(function(number) {
-            return racunalnikStevilke.includes(number);
-        });
-
-        if (allNumbersDrawn) {
-            alert("Računalnik je zmagal");
-            ustaviTimer();
-        }
-    }
-
-
-
    ustaviTimer();
 }
 function racunalnikPreveriZmago()
 {
     let racunalnikZ = preveriPetZaporednih("racunalnik", "green");
     if (racunalnikZ) {
+        zvok('lose.wav');
         alert("Računalnik je zmagal");
     }
 }
@@ -272,29 +222,45 @@ function preveriPetZaporednih(tableId, color) {
     let table = document.getElementById(tableId);
     let vrstice = table.querySelectorAll("tr");
 
-    // Check rows
     for (let i = 0; i < vrstice.length; i++) {
         let cells = vrstice[i].querySelectorAll("td");
-        let consecutiveCount = 0;
+        let stej = 0;
         for (let j = 0; j < cells.length; j++) {
             if (cells[j].style.backgroundColor === color) {
-                consecutiveCount++;
-                if (consecutiveCount === 5) {
-                    return true; // Found 5 consecutive cells
+                stej++;
+                if (stej === 5) {
+                    return true;
                 }
             } else {
-                consecutiveCount = 0; // Reset consecutive count if the cell is not the specified color
+                stej = 0;
             }
         }
     }
-
-    // No 5 consecutive cells found
     return false;
 }
-
 function  zvok(url)
 {
    let audio = document.getElementById("zvok");
    audio.src = url;
    audio.play();
+}
+function pozeniTimer(){
+    interval = setInterval(function(){izpisCasa();}, 1000);
+    let klik_aktiven=1;
+}
+let klik_aktiven=0;
+function ustaviTimer(){
+    clearInterval(interval);
+    let klik_aktiven=0;
+}
+
+let cas = 0;
+function izpisCasa(){
+    cas++;
+    let sec = cas % 60;
+    let  min = (cas - sec)/60;
+    min += ':'
+    if(sec < 10)min+='0';
+    min += sec;
+    document.getElementById('casIgranja').innerHTML = min;
 }
