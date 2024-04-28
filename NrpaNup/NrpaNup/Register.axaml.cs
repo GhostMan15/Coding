8466b7b367d797c0ca15a58cb7ca4afcafb7aba3
GhostMan15 <farukuzicanin1@gmail.com>
2024-04-28 11:14:17 +0200

    dodan shit

---
+++ NrpaNup/NrpaNup/Register.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace NrpaNup;
+
+public partial class Register : Window
+{
+    public Register()
+    {
+        InitializeComponent();
+    }
+}
