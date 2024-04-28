00608bc3d1bf7bb8d2721435564e88c75006c9de
GhostMan15 <farukuzicanin1@gmail.com>
2024-04-28 18:59:13 +0200

    dodan shit

---
+++ NrpaNup/NrpaNup/Admin.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace NrpaNup;
+
+public partial class Admin : Window
+{
+    public Admin()
+    {
+        InitializeComponent();
+    }
+}
---
+++ NrpaNup/NrpaNup/Uporabnik.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace NrpaNup;
+
+public partial class Uporabnik : Window
+{
+    public Uporabnik()
+    {
+        InitializeComponent();
+    }
+}
