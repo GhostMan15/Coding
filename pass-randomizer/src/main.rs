7876a4e71554f6e18b02db8fead00be7a45ed059
GhostMan15 <farukuzicanin1@gmail.com>
2024-08-12 16:47:12 +0200

    urjeanje

--- pass-randomizer/src/main.rs
+++ pass-randomizer/src/main.rs
@@ -3,23 +3,23 @@ use std::{char, fs::{self, OpenOptions}};
 use rand::Rng;
 use std::io::Write;
 
-fn main(){
-    const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789)(*?!#$%&=";
-    const geslo_len: u8 = 6; 
-    let pot = "/home/faruk/Documents/GitHub/Coding/pass-randomizer/gesla";
-      //Kreiranje gesla
-    let mut rng = rand::thread_rng();
-    let password: String = (0..geslo_len).map(|_|{
-        let idx = rng.gen_range(0..CHARSET.len());
-        CHARSET[idx] as char
+
+fn randomizer(charset :&[u8],  dolzina:u8) -> String{
+ let mut rng = rand::thread_rng();
+    let password: String = (0..dolzina).map(|_|{
+        let idx = rng.gen_range(0..charset.len());
+        charset[idx] as char
     })
     .collect();
+    return password;
+}
 
-
-    //Če že obstaja geslo ponovno rekreiranje
-    if password == pot{
-        let _ = password;
-    }
+fn main(){
+    const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789)(*?!#$%&=";
+    const GESLO_LEN: u8 = 6; 
+    let pot = "/home/faruk/Documents/GitHub/Coding/pass-randomizer/gesla";
+      //Kreiranje gesla
+    let ustvari = randomizer(CHARSET, GESLO_LEN);
 
     //Zapisovanje gesla med ostala gesla 
     let mut file = OpenOptions::new()
@@ -27,7 +27,12 @@ fn main(){
         .create(true)
         .open(pot)
         .expect("Ne morm ofnat"); 
-    write!(file,"{}\n", password).expect("Ne delujeoč");
-    let contet = fs::read_to_string(pot);
+    write!(file,"{}\n", ustvari).expect("Ne delujeoč");
+    let contet = fs::read_to_string(pot).expect("Ni fajla");
+
+    //Če že obstaja geslo ponovno rekreiranje
+    if contet == ustvari{
+        let _ = ustvari;
+    }
     println!("{:?}", contet);
 }
