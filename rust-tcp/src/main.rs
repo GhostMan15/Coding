3375c3b6078ae2dfe85eaa28e8e57ac48feb7124
GhostMan15 <farukuzicanin1@gmail.com>
2024-11-13 22:15:15 +0100

    ye

--- rust-tcp/src/main.rs
+++ rust-tcp/src/main.rs
@@ -15,7 +15,6 @@ fn handle_client(mut stream: TcpStream){
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").expect("Failal se povezat");
     println!("Server poslusa");
-
     for stream in listener.incoming(){
         match stream {
             Ok(stream) => {
