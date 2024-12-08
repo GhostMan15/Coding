ed2c5ddd9631bcf66501e8f63cf4c5e4f73e38e4
GhostMan15 <farukuzicanin1@gmail.com>
2024-12-08 15:51:53 +0100

    yur

--- AOC2024/DAY3/3.c
+++ AOC2024/DAY3/3.c
@@ -1,24 +1,34 @@
-
+#include <ctype.h>
+#include <stdbool.h>
 #include <stdio.h>
 #define MAX_SIZE 1000000
 int isDigit(char c) {
-  return c >= '0' && c <= '9';
+  return isdigit(c);
 }
+
 int main(int argc, char *argv[])
 {
   int m = 0;
   int rez = 0;
-  int rez2 = 0;
+  bool lahka = true;
   char arr[MAX_SIZE];
-  FILE *input = fopen("../DAY3/3.1.txt", "r"); 
+  FILE *input = fopen("../DAY3/3.txt", "r"); 
   while (m < MAX_SIZE - 1 &&fscanf(input, "%c", &arr[m])==1) {
     m++;
   }
   arr[m] = '\0';
   fclose(input);
-   
-  for(int i = 0; i <= m; i++){
-    if(arr[i] == 'm' && arr[i+1] == 'u' && arr[i+2] == 'l' && arr[i+3] == '(') {
+  for(int i = 0; i < m - 7; i++){    
+    if (arr[i] == 'd' && arr[i+1] == 'o' && arr[i+2] == '(' && arr[i+3] == ')') {
+      lahka = true;
+      printf("Multi enabled at index %d (found do_)\n", i);
+    }
+    if (arr[i] == 'd' && arr[i+1] == 'o' && arr[i+2] == 'n' && arr[i+3] == '\'' 
+      && arr[i+4] == 't' && arr[i+5] == '(' && arr[i+6] == ')') {
+      lahka = false;
+      printf("Multi disabled at index %d (found do_not)\n", i);
+    } 
+    if(lahka  && i +3 < m &&arr[i] == 'm' && arr[i+1] == 'u' && arr[i+2] == 'l' && arr[i+3] == '(') {
       int a=0;
       int b=0;
       int j= i+4;
@@ -28,8 +38,6 @@ int main(int argc, char *argv[])
       }
       if(arr[j] ==','){
         j++;
-      }else {
-        continue;
       }
       while(isDigit(arr[j])) {
         b =b *10 +(arr[j] - '0');
@@ -37,10 +45,11 @@ int main(int argc, char *argv[])
       }
       if(arr[j] == ')'){
         rez += a*b;
-      }      
+      }else {
+        printf("ni slo");
+      } 
     }
   }
-
   printf("%d", rez);
   return 0;
 }
